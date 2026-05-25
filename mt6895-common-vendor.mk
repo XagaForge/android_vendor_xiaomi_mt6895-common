236cf44601c2d38473688e9277f868bbef44407c
ChrisCatto <chriscatto@proton.me>
2026-05-25 14:22:55 +0300

    Revert "mt6895-common: Let's go full prebuilt for C2 vendor stack"
    * Only causes more issues.
    
    This reverts commit efd70d4350e27b94e2ed5c5d18d08e30ef5d8644.
    
    Change-Id: Ia19b8bc3b4caf82fbe58aa8d3054008d1b8cddc9
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- mt6895-common-vendor.mk
+++ mt6895-common-vendor.mk
@@ -247,15 +247,9 @@ PRODUCT_PACKAGES += \
     libccci_util \
     libcmdl \
     libcmdl_ndk.mtk.vndk \
-    libcodec2_hidl@1.0-v31 \
-    libcodec2_hidl@1.1-v31 \
-    libcodec2_hidl@1.2-v31 \
-    libcodec2_hidl_plugin-v31 \
     libcodec2_mtk_c2store \
     libcodec2_mtk_vdec \
     libcodec2_mtk_venc \
-    libcodec2_soft_common-v31 \
-    libcodec2_vndk-v31 \
     libcodec2_vpp_dolby_plugin \
     libcodec2_vpp_dump_mtk_yuv_plugin \
     libcodec2_vpp_gc_plugin \
@@ -353,11 +347,9 @@ PRODUCT_PACKAGES += \
     librilfusion \
     librpc \
     librt_extamp_intf \
-    libsfplugin_ccodec_utils-v31 \
     libsn100u_fw \
     libspeech_enh_lib \
     libspeechparser_vendor \
-    libstagefright_bufferqueue_helper-v35 \
     libstorage_otp \
     libsysenv \
     libteeclientjni_isee \
